3295ba13d26a4fb9e932910fd341ab458705b0db
Ritvik Upadhyaya <ritvikupadhyaya@gmail.com>
2018-10-13 16:08:35 -0700

    Setup the basic react native environment

--- README.md
+++
@@ -1,2 +0,0 @@
-#CS Website
-##This repo will contain source for my personal website
--- js/app.js
+++
@@ -1,3 +0,0 @@
-/*Load the JavaScript for the page*/
-/*particleJS.load() loads the preferences for the particleJS  stored in a JSON file*/
-particlesJS.load('particles-js', 'js/particles.json', function() {});
